b09caf7757b529b5831d7a5501308c044bad441f
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-16 21:34:17 +0530

    Implement stack

---
+++ src/Structures/Linear/Stack/Stack.ts
@@ -0,0 +1,29 @@
+import { defaultEqualsFunction, EqualsFunction } from '../../../utils'
+import { LinkedList } from '../LinkedList'
+
+export class Stack<T> implements Iterable<T> {
+    list: LinkedList<T>
+
+    constructor(equalsFn: EqualsFunction = defaultEqualsFunction) {
+        this.list = new LinkedList(equalsFn)
+    }
+
+    get size(): number {
+        return this.list.size
+    }
+
+    push = (value: T): Stack<T> => {
+        this.list.addBack(value)
+        return this
+    }
+
+    pop = (): T | undefined => this.list.removeBack()
+
+    peek = (): T | undefined => this.list.peakBack()
+
+    isEmpty = (): boolean => this.list.size === 0;
+
+    [Symbol.iterator](): Iterator<T> {
+        return this.list[Symbol.iterator]()
+    }
+}
